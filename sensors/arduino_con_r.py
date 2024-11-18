d3770bb00df8e14d9e10c14547aa51564947fd69
Leslie343 <legomouse@naver.com>
2024-11-18 20:32:51 +0900

    rev

--- sensors/arduino_con_r.py
+++ sensors/arduino_con_r.py
@@ -17,16 +17,22 @@ try:
         # 시리얼 포트에서 한 줄을 읽음
         line = ser.readline().decode('utf-8').strip()
         print("읽은 데이터는 다음과 같습니다:", line)
-        if line[0] == "h":
+        if line == (None or ""):
+            pass
+        elif line[0] == "h":
             heart_i = np.append(heart_i, int(line[1:]))
         elif line[0] == "a":
             at = float(line[1:])
         elif line[0] == "n":
             ng = float(line[1:])
-        data = {"위도":at, "경도":ng}
+        data = {"latitude":at, "longitude":ng}
         print(data)
         heart_i = heart_i.astype(int)
         print(heart_i)
+        with open("data.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)  # indent=4로 보기 좋게 포맷
+except IndexError as e:
+    print(f"인덱스 오류 발생: {e}")
 except KeyboardInterrupt:
     print("프로그램을 종료합니다.")
 finally:
