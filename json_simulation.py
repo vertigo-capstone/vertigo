816779af8ccf808db12e7a293fb8457f3cb5b31d
Leslie343 <legomouse@naver.com>
2024-11-19 16:58:15 +0900

    Update json_simulation.py
    The code work always.

--- json_simulation.py
+++ json_simulation.py
@@ -1,15 +1,16 @@
 import json
 import time
 
-asdf = time.time()
-data = {
-    "trash0": "name",
-    "trash1": 30,
-    "trash2": "New York",
-    "trash3": False,
-    "data": asdf
-}
-
-# JSON 파일로 저장
-with open('data.json', 'w') as json_file: #파일 경로 지정할 것
-    json.dump(data, json_file, indent=4)  # indent는 읽기 쉽게 들여쓰기를 추가
+while True:
+    asdf = time.time()
+    data = {
+        "trash0": "name",
+        "trash1": 30,
+        "trash2": "New York",
+        "trash3": False,
+        "data": asdf
+    }
+    # JSON 파일로 저장
+    with open('C://Users//PKNU//simple_data//data.json', 'w') as json_file: #파일 경로 지정할 것
+        json.dump(data, json_file, indent=4)  # indent는 읽기 쉽게 들여쓰기를 추가
+    time.sleep(1)
