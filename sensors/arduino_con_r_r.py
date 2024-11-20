90e4dda592a8497e6af18d33d09e55223a4145c5
Leslie343 <legomouse@naver.com>
2024-11-20 19:02:41 +0900

    Fixed incorrect reference.

--- sensors/arduino_con_r_r.py
+++ sensors/arduino_con_r_r.py
@@ -3,29 +3,26 @@ import serial
 import json
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
-# 아두이노와 연결된 시리얼 포트 설정 (예: COM3 또는 /dev/ttyUSB0 등)
-arduino_port = '/dev/ttyACM0'  # Windows의 경우 'COM3' 또는 아두이노 포트를 설정
-baud_rate = 9600  # 아두이노와 동일한 baud_rate 설정
-
+# 아두이노와 연결된 시리얼 포트 설정
+arduino_port = '/dev/ttyACM0'  #포트 주소
+baud_rate = 9600  # 아두이노와 동일한 baud_rate로 설정
 # 시리얼 포트 열기
-ser = serial.Serial(arduino_port, baud_rate)
 heart_i = np.array([])
 at = None
 ng = None
-loc = "/home/vertigo/vertigo/sensors/information_data/location.json"
+loc = "/home/vertigo/vertigo/sensors/information_data/gps_data.json"
 #json 쓰기 함수
 def json_write(let_loc, let_data):
     with open(let_loc, "w") as json_file:
             json.dump(let_data, json_file, indent=4)  # indent=4로 보기 좋게 포맷
 #시리얼 읽기 함수
 def ser_read(let_ser):
-    let_line = ser.readline().decode('utf-8').strip()
+    let_line = let_ser.readline().decode('utf-8').strip()
     return let_line
 # 시리얼 데이터를 읽고 처리
 try:
     while True:
-        line = ser_read(loc)
+        line = ser_read(serial.Serial(arduino_port, baud_rate))
         print("읽은 데이터는 다음과 같습니다:", line)
         if line == (None or ""):
             pass
@@ -45,4 +42,4 @@ try:
 except KeyboardInterrupt:
     print("프로그램을 종료합니다.")
 finally:
-    ser.close()  # 시리얼 포트 닫기
+    serial.Serial(arduino_port, baud_rate).close()  # 시리얼 포트 닫기
