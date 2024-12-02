2b971b35175b8f1b8390fba43a05ff9867bbc524
yeeeon02 <yeonn128902@naver.com>
2024-12-02 20:23:02 +0900

    스크립트 자동 실행
    - start_all.sh : 자동 실행할 스크립트 파일 지정
    - start_all.service : 경로 지정, 실행

---
+++ start_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 자동 실행할 스크립트 파일
+# python3 ./scripts/script1.py &
+python3 ./sensors/script0_ver0.py &
+python3 ./sensors/script1_ver1.py &
--- web/app.py
+++ web/app.py
@@ -3,7 +3,8 @@ from firebase_admin import credentials, firestore
 from flask import Flask, render_template, request, redirect, url_for, session
 
 # Firebase Admin SDK 초기화
-cred = credentials.Certificate('firebase-key.json')  # JSON 파일 경로
+cred = credentials.Certificate(
+    "./firebase/vertigo-firebase.json")  # JSON 파일 경로
 firebase_admin.initialize_app(cred)
 
 # Flask 앱 초기화
@@ -11,22 +12,24 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # 세션을 위한 비밀 키 설정
 
 # Firestore에서 센서 데이터를 가져오는 함수
+
+
 def get_sensor_data():
     db = firestore.client()  # Firestore 클라이언트 생성
 
     # 'sensor' 컬렉션에서 각 문서 가져오기
     sensor_data = {}
     try:
-        gyroscope_doc = db.collection('sensor').document('gyroscope').get()
-        heart_doc = db.collection('sensor').document('heart').get()
-        gps_doc = db.collection('sensor').document('GPS').get()
+        gyroscope_doc = db.collection('vertigo').document('vertigo').get()
+        # heart_doc = db.collection('sensor').document('heart').get()
+        # gps_doc = db.collection('sensor').document('GPS').get()
 
         if gyroscope_doc.exists:
             sensor_data['gyroscope'] = gyroscope_doc.to_dict()
-        if heart_doc.exists:
-            sensor_data['heart'] = heart_doc.to_dict()
-        if gps_doc.exists:
-            sensor_data['GPS'] = gps_doc.to_dict()
+        # if heart_doc.exists:
+        #     sensor_data['heart'] = heart_doc.to_dict()
+        # if gps_doc.exists:
+        #     sensor_data['GPS'] = gps_doc.to_dict()
 
         print("Sensor data:", sensor_data)  # 데이터 확인
     except Exception as e:
@@ -41,6 +44,8 @@ def login():
     return render_template('login.html', message="")
 
 # 로그인 처리
+
+
 @app.route('/login', methods=['POST'])
 def do_login():
     username = request.form['username']
@@ -51,9 +56,12 @@ def do_login():
         session['username'] = username  # 세션에 사용자 정보 저장
         return redirect(url_for('dashboard'))  # 대시보드로 리다이렉트
     else:
-        return render_template('login.html', message="로그인에 실패하였습니다.")  # 로그인 실패 메시지
+        # 로그인 실패 메시지
+        return render_template('login.html', message="로그인에 실패하였습니다.")
 
 # 대시보드 페이지
+
+
 @app.route('/dashboard')
 def dashboard():
     if 'username' not in session:
@@ -63,7 +71,9 @@ def dashboard():
     sensor_data = get_sensor_data()  # Firestore에서 센서 데이터 가져오기
     if sensor_data is None:
         sensor_data = {}  # 데이터가 없으면 빈 딕셔너리로 대체
-    return render_template('dashboard.html', username=username, sensor_data=sensor_data)  # 대시보드에 데이터 출력
+    # 대시보드에 데이터 출력
+    return render_template('dashboard.html', username=username, sensor_data=sensor_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
